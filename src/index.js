d2ca908541d769a6fc5e459adaa1a5a036bc1721
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-08-17 10:31:53 +0530

    Added basic User Model by using npx sequelize model:generate --name User --attributes email:string,password:string this command

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const apiRouter = require("./routes/index");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
 app.use("/api", apiRouter);
 
 app.listen(ServerConfig.PORT, () => {
